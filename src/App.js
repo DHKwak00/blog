2be82ba7f3b11eb4894f2ae0c0d6dac1c8aac5d4
DHKwak00 <andreas0370@gmail.com>
2024-07-09 17:51:47 +0900

    new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬 독학',
+  ]);
+  let [따봉, 따봉변경] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        글제목변경(copy.sort());
+      }}>가나다순 정렬</button>
+
+      <button 
+        onClick={()=>{
+        // 글제목[0] = '여자 코트 추천';
+        // 글제목변경(글제목); -> array나 obj는 초기화값을 보존하자
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천'
+        글제목변경(copy);
+      }}>버튼</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <p>7월 7일 발행</p>
+      </div>
+
+
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>7월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>7월 9일 발행</p>
+      </div>
+    </div>
+  ); //end return
+}
+
+export default App;
